6604fc36510caae04980c32290918cac17ba668d
ymd <1946334058@qq.com>
2020-05-24 23:24:30 +0800

    1-4章基础完成

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+//将RestTemplate注入（用于两个微服务模块之间的调用请求），需要先把RestTemplate注入进来
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
